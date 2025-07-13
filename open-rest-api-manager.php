2c03002f4e7c957efc7efd8ea5424567dfcf2ec2
jwnukoski <jwnukoski@protonmail.com>
2025-07-13 12:09:44 -0400

    init

---
+++ open-rest-api-manager.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Open REST API Manager
+ *
+ * @package       Open_REST_API_Manager
+ * @author        Joe Wnukoski
+ * @version       1.0.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:   Open REST API Manager
+ * Plugin URI:    https://github.com/jwnukoski/wp-open-rest-api-manager
+ * Description:   Open source REST API manager for WordPress.
+ * Version:       1.0.0
+ * Author:        Joe Wnukoski
+ * Author URI:    https://jwnukoski.com
+ * Text Domain:   open-rest-api-manager
+ * Domain Path:   /languages
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Include your custom code here.
